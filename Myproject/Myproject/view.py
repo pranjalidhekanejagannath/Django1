c8ef7c55369e5191dde174faee5e64c74d765c28
pranjalidhekane <pranjalidhekane2002@gmail.com>
2025-08-25 13:22:49 +0530

    first commit

---
+++ Myproject/Myproject/view.py
@@ -0,0 +1,11 @@
+# from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def homepage(request):
+    # return HttpResponse("Hello Word! I'm Home.")
+    return render(request, 'home.html')
+
+def about(request):
+    # return HttpResponse("My About Page.")
+    return render(request, 'about.html')
